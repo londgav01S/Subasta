339b3ac4ae8269db83780d266649f0fa9ba6e80e
londgav01S <santiago.londonog@uqvirtual.edu.co>
2023-11-18 21:58:20 -0500

    otro commit, implementacion de boton Usuario

--- src/main/java/co/edu/uniquindio/subasta/viewController/UserViewController.java
+++ src/main/java/co/edu/uniquindio/subasta/viewController/UserViewController.java
@@ -1,17 +1,24 @@
 package co.edu.uniquindio.subasta.viewController;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 public class UserViewController {
 
     @FXML
-    private ComboBox<?> cbTipoUsuario;
+    private Button btActualizarDatos;
+
+    @FXML
+    private Button btElimanar;
 
     @FXML
     private TextField fContrasenia;
+
+    @FXML
+    private TextField fContraseniaNuevo;
 
     @FXML
     private TextField fCorreoElectronico;
@@ -30,5 +37,15 @@ public class UserViewController {
 
     @FXML
     private DatePicker tfFechaNacimiento;
+
+    @FXML
+    void actualizar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarUsuario(ActionEvent event) {
+
+    }
 
 }
